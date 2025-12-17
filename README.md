fde2a87255f433279353c143d21bd0a9f1d23745
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-12-17 12:01:45 +0700

    feat: output

---
+++ README.md
@@ -0,0 +1,20 @@
+# Quran Data
+
+Sumber : https://web-api.qurankemenag.net
+
+- Daftar surah : `/output/chapters.csv`
+- Daftar ayat : `/output/verses.csv`
+
+## Perbarui Data
+
+Instal package
+
+```bash
+npm install
+```
+
+Ambil data
+
+```bash
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,55 @@
+const axios = require('axios')
+const { json2csv } = require('json-2-csv');
+const fs = require('fs')
+
+const chapterPath = 'https://web-api.qurankemenag.net/quran-surah'
+const versePath = 'https://web-api.qurankemenag.net/quran-ayah'
+
+async function generateChapters() {
+  const res = await axios.get(chapterPath, {
+    headers: {
+      'Origin': 'https://quran.kemenag.go.id'
+    }
+  })
+
+  const csv = await json2csv(res.data.data, { excludeKeys: ['updated_at'] })
+
+  if (!fs.existsSync('output')) {
+    await fs.promises.mkdir('output')
+  }
+
+  await fs.promises.writeFile('output/chapters.csv', csv)
+}
+
+async function generateVerses() {
+  const chapters = Array.from({ length: 114 }, (_, i) => i + 1)
+
+  for (const chapter of chapters) {
+    const res = await axios.get(versePath, {
+      headers: {
+        'Origin': 'https://quran.kemenag.go.id'
+      },
+      params: {
+        surah: chapter
+      }
+    })
+
+    if (chapter === 1) {
+      await fs.promises.rm('output/verses.csv')
+    }
+
+    const csv = await json2csv(res.data.data, {
+      prependHeader: chapter === 1,
+      excludeKeys: ['surah', 'updated_at']
+    })
+
+    if (!fs.existsSync('output')) {
+      await fs.promises.mkdir('output')
+    }
+
+    await fs.promises.appendFile('output/verses.csv', csv)
+  }
+}
+
+generateChapters()
+generateVerses()
